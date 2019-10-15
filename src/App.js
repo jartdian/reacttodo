86d1bf1a1b85329352b0607235759c4824a362b6
jan <46709199+jartdian@users.noreply.github.com>
2019-10-15 15:08:11 +0200

    added todo form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import TodoList from "./Components/TodoList";
 
 export default () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 };
